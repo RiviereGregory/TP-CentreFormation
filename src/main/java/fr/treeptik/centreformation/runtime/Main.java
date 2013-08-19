0138b68ab671667f69d9c973ec7e4d5cbfcb3d2c
RIVIERE Gregory <gregory.riviere@laposte.net>
2013-08-19 11:55:36 +0200

    test eclipse

--- src/main/java/fr/treeptik/centreformation/runtime/Main.java
+++ src/main/java/fr/treeptik/centreformation/runtime/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		// Ajout commentaire eclipse
+		// Ajout commentaire eclipse 2
 		System.out.println("*");
 		System.out.println("*");
 		System.out.println("*");
